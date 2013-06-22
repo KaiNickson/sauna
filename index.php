0eebfa6613adf4e88f156522ce7dd6f3507e8b08
Sammi Fux <sammi.fux@student.ehb.be>
2013-06-22 16:43:34 +0200

    Revert "Revert "Root aangemaakt en index aangepast""
    This reverts commit 40fb69abb29daf92feae2ae04863aeabec9428e6.

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Sauna</title>
-<script src="http://code.jquery.com/jquery-1.10.1.js"></script>
-<script>
-	$(document).ready(function(e) {
-       $.ajax({
-			url:"Sauna.php",
-			dataType:"html",
-			success: function(html) {
-				$('body').html(html);
-			}
-		}); 
-    });
-</script>
-</head>
-
-<body>
-</body>
-</html>
+<?php
+require_once 'root.php';
+header('Location: ' . $PROJECTROOT . 'Sauna.php');
+?>
