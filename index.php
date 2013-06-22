0ff54a3ee2576bde8416800e2c901e2098fe7c16
Sammi Fux <sammi.fux@student.ehb.be>
2013-06-22 16:47:48 +0200

    Revert "Revert "Revert "Revert "Index aangepast zodat deze via ajax Sauna.php ophaalt""""
    This reverts commit b38d1c1d7ad17935ab1619982116656d4704bd2f.

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
-<?php
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Sauna</title>
+<script src="http://code.jquery.com/jquery-1.10.1.js"></script>
+<script>
+	$(document).ready(function(e) {
+       $.ajax({
+			url:"Sauna.php",
+			dataType:"html",
+			success: function(html) {
+				$('body').html(html);
+			}
+		}); 
+    });
+</script>
+</head>
 
-header('Location: Sauna.php');
-?>
+<body>
+</body>
+</html>
